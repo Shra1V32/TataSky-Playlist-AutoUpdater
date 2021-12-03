030c8939080583c880ea8e0b6777ad5ba59a90b1
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2021-12-03 14:30:37 +0530

    Setup: explicitly install packages

--- setup_termux.sh
+++ setup_termux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pkg install gh & pkg install expect & pkg install tput & pkg install python3 && pip install requests 
+pkg install gh -y & pkg install expect -y & pkg install tput -y & pkg install python3 -y && pip install requests -y 
