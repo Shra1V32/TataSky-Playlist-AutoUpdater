fe1248fb3c3a5a914a0b2784dba77bef3964150c
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2021-12-04 11:55:08 +0530

    Script: Optimise

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt install python3 & apt install python-pip && pip install requests & apt install expect
+apt install python3 expect && pip install requests
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+apt update
+apt install gh
