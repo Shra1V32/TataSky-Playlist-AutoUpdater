ad0b0ae4437d5d0e4e5a321c1b7180fa0133537a
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2021-12-19 14:24:57 +0530

    Script: Warn the error and exit if setup fails

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
 if [[ $OSTYPE == 'linux-gnu'* ]]; then
 sudo echo '' > /dev/null 2>&1
-sudo apt install python3 expect && pip install requests
+sudo apt install python3 expect && pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
@@ -9,6 +11,6 @@ sudo apt install gh
 fi
 
 if [[ $OSTYPE == 'linux-android'* ]]; then
-pkg install gh ncurses-utils expect python gettext -y
-pip install requests
+pkg install gh ncurses-utils expect python gettext -y || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
+pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 fi
