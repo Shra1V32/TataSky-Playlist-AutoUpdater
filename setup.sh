3194b0634c2c3b4ccc0df7e3667e698895d8ecea
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2021-12-24 23:55:39 +0530

    Setup: Adapt to previous commits

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 RED='\033[0;31m'
 NC='\033[0m'
 if [[ $OSTYPE == 'linux-gnu'* ]]; then
+echo "Please wait while the installation takes place..."
 sudo echo '' > /dev/null 2>&1
 sudo apt install python3 expect && pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -11,6 +12,9 @@ sudo apt install gh
 fi
 
 if [[ $OSTYPE == 'linux-android'* ]]; then
-pkg install gh ncurses-utils expect python gettext -y || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
+echo "Please wait while the installation takes place..."
+pkg install git gh ncurses-utils expect python gettext -y || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 fi
+
+echo "Installation done successfully, Executing the main.sh script now..."
