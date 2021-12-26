129679997c2e9ad25288d1d399a843d63f20578d
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2021-12-26 20:14:02 +0530

    Compatibility: Warn and exit for unsupported platforms

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 RED='\033[0;31m'
 NC='\033[0m'
 if [[ $OSTYPE == 'linux-gnu'* ]]; then
 echo "Please wait while the installation takes place..."
+printf "Please Enter your password to proceed with the setup: "
 sudo echo '' > /dev/null 2>&1
 sudo apt update
 sudo apt install python3 python3-pip expect && pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
@@ -11,11 +12,12 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt update
 sudo apt install gh
 echo "Installation done successfully, Executing the main.sh script now..."
-fi
 
-if [[ $OSTYPE == 'linux-android'* ]]; then
+elif [[ $OSTYPE == 'linux-android'* ]]; then
 echo "Please wait while the installation takes place..."
 pkg install git gh ncurses-utils expect python gettext -y || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 echo "Installation done successfully, Executing the main.sh script now..."
+else
+echo -e "${RED}Platform not supported, Exiting...${NC}"; exit 1;
 fi
