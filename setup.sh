665f17fe6b75b5f2775bcd4955fbfbc429bf14ed
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2022-01-04 19:00:32 +0530

    Setup: Explicitly install packages

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo "Please wait while the installation takes place..."
 printf "Please Enter your password to proceed with the setup: "
 sudo echo '' > /dev/null 2>&1
 sudo apt update
-sudo apt install python3 python3-pip expect && pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
+sudo apt install python3 python3-pip expect -y && pip install requests || { echo -e "${RED}Something went wrong, Try running the script again.${NC}"; exit 1; }
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
