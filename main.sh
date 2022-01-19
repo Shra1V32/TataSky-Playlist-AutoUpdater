c36035642786d2717f6b9378765e1f09d676ed97
Shravan Vadeghar <89348492+Shra1V32@users.noreply.github.com>
2022-01-19 12:10:50 +0530

    Script: Save creds after playlist gen

--- main.sh
+++ main.sh
@@ -30,7 +30,6 @@ take_vars()
 {
 if [[ ! -f "$LOCALDIR/.usercreds" ]]; then
 take_input;
-printf "sub_id=$sub_id\ntata_mobile=$tata_mobile\ntata_pass=$tata_pass\ngit_mail=$git_mail\ngit_id=$git_id\ngit_token=$git_token\n" > .usercreds
 else
 printf "\nLooks like you've already entered the details, Taking the data from .usercreds so that you won't have to enter the details again...\n"
 source ./.usercreds
@@ -105,6 +104,8 @@ cd ${dir} && rm allChannelPlaylist.m3u && mv ../code_samples/allChannelPlaylist.
 git add .
 git commit -m "Initial Playlist Upload"
 git push >> /dev/null 2>&1 || { tput setaf 9; printf 'Something went wrong!\n ERROR Code: 65x00a\n'; exit 1; }
+echo "Saving usercreds so that you don't have to login again..."
+printf "sub_id=$sub_id\ntata_mobile=$tata_mobile\ntata_pass=$tata_pass\ngit_mail=$git_mail\ngit_id=$git_id\ngit_token=$git_token\n" > .usercreds
 tput setaf 51; echo "Successfully created your new private repo." && printf "Check your new private repo here: ${NC}https://github.com/$git_id/TataSkyIPTV-Daily\n" && tput setaf 51; printf "Check Your Playlist URL here: ${NC}https://gist.githubusercontent.com/$git_id/$dir/raw/allChannelPlaylist.m3u \n" && tput setaf 51; printf "You can directly paste this URL in Tivimate/OTT Navigator now, No need to remove hashcode\n"
 tput bold; printf "\n\nFor Privacy Reasons, NEVER SHARE your GitHub Tokens, Tata Sky Account Credentials and Playlist URL TO ANYONE. \n"
 tput setaf 51; printf "Using this script for Commercial uses is NOT PERMITTED. \n\n"
@@ -112,6 +113,6 @@ tput setaf 51; echo "Script by Shra1V32, Please do star my repo if you've liked 
 tput setaf 51; echo "Credits: Gaurav Thakkar (https://github.com/ForceGT) & Manohar Kumar"
 tput setaf 51; echo "My Github Profile: https://github.com/Shra1V32"
 printf '\n\n'
-rm -rf $LOCALDIR/Tata-Sky-IPTV
+rm -rf $LOCALDIR/Tata-Sky-IPTV;
 echo "Press Enter to exit."; read junk;
 tput setaf init;
