d744e545aa3debda280107b8ddd2a60ea955f83e
Nageshwar128 <namanageshwar@outlook.com>
2022-02-02 13:45:36 +0530

    Rewrite steps on 'How to use?'

---
+++ curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+if [[ $OSTYPE == 'linux-android'* ]]; then
+pkg install git; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
+elif [[ $OSTYPE == 'linux-gnu'* ]]; then
+git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; cd TataSky-Playlist-AutoUpdater; bash main.sh
+fi
