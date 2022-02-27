ae3a70e46beff887783b8480b51e2731f2e0928b
Shra1V32 <namanageshwar@outlook.com>
2022-02-28 00:02:55 +0530

    curl.sh: Fix minor issues

--- curl.sh
+++ curl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 if [[ $OSTYPE == 'linux-android'* ]]; then
 pkg install git
-git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; || { rm -rf TataSky-Playlist-AutoUpdater; }
+git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
 cd TataSky-Playlist-AutoUpdater;
-bash main.sh
+./main.sh
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
-git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater;
+git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
 cd TataSky-Playlist-AutoUpdater;
-bash main.sh
+./main.sh
 fi
