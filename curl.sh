f96b7f2da9c5ce46794d2cfc84c4f70d4fb35ea7
Shra1V32 <namanageshwar@outlook.com>
2022-03-26 13:04:56 +0530

    curl.sh: exit if termux version is not supported

--- curl.sh
+++ curl.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -e
-if [[ $OSTYPE == 'linux-android'* ]]; then
-pkg install git
-git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
-cd TataSky-Playlist-AutoUpdater;
-./main.sh
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [[ $OSTYPE == 'linux-android'* && $(echo "$TERMUX_VERSION" | cut -c 3-5) -ge "117" ]]; then
+    pkg install git
+    git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
+    cd TataSky-Playlist-AutoUpdater;
+    ./main.sh
+
 elif [[ $OSTYPE == 'linux-gnu'* ]]; then
-sudo apt install git
-git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
-cd TataSky-Playlist-AutoUpdater;
-./main.sh
+    sudo apt install git
+    git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater || { rm -rf TataSky-Playlist-AutoUpdater; git clone https://github.com/Shra1V32/TataSky-Playlist-AutoUpdater; }
+    cd TataSky-Playlist-AutoUpdater;
+    ./main.sh
+elif [[ $(echo "$TERMUX_VERSION" | cut -c 3-5) -le "117" ]]; then
+    echo -e "Please use Latest Termux release, i.e, from FDroid (https://f-droid.org/en/packages/com.termux/)";
+    exit 1;
+else
+    echo "Platform not supported"
+    exit 1;
 fi
