8c3b0fe3644bdd27c6a7ed8adb27085b302a1fd7
Shra1V32 <namanageshwar@outlook.com>
2022-08-28 23:30:55 +0530

    validate_git.py: reformat code

--- validate_git.py
+++ validate_git.py
@@ -6,21 +6,25 @@ import requests
 import json
 import datetime
 
+
 class PeriodNotCompleted(Exception):
     pass
 
 
-if sys.argv[1]!='':
-    data=requests.get(sys.argv[1])
-    current_date=requests.get("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+if sys.argv[1] != '':
+    data = requests.get(sys.argv[1])
+    current_date = requests.get(
+        "http://worldtimeapi.org/api/timezone/Asia/Kolkata")
     a_year, a_month, a_day = current_date.json()["datetime"][:10].split('-')
-    a_hours, a_minutes, a_seconds = current_date.json()["datetime"][11:19].split(':')
-    #print(a_hours,a_minutes,a_seconds)
-    api_data=data.json()
-    creation_date=api_data["created_at"]
+    a_hours, a_minutes, a_seconds = current_date.json()[
+        "datetime"][11:19].split(':')
+    # print(a_hours,a_minutes,a_seconds)
+    api_data = data.json()
+    creation_date = api_data["created_at"]
     u_year, u_month, u_day = creation_date[:10].split('-')
     u_hours, u_minutes, u_seconds = creation_date[11:19].split(':')
-    ts= (datetime.datetime(int(a_year),int(a_month),int(a_day),int(a_hours),int(a_minutes),int(a_seconds)) - datetime.datetime(int(u_year),int(u_month),int(u_day),int(u_hours),int(u_minutes),int(u_seconds))).total_seconds()
+    ts = (datetime.datetime(int(a_year), int(a_month), int(a_day), int(a_hours), int(a_minutes), int(a_seconds)) -
+          datetime.datetime(int(u_year), int(u_month), int(u_day), int(u_hours), int(u_minutes), int(u_seconds))).total_seconds()
     if int(ts) < 1209600:
         raise PeriodNotCompleted("A Period of 14 is not completed")
 else:
