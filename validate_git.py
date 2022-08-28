16966051b00b61cfbb463b7fd0231c46a820dfa3
Shra1V32 <namanageshwar@outlook.com>
2022-08-28 23:36:17 +0530

    validate_git.py: print out remaining time for account eligibility

--- validate_git.py
+++ validate_git.py
@@ -25,8 +25,14 @@ if sys.argv[1] != '':
     u_hours, u_minutes, u_seconds = creation_date[11:19].split(':')
     ts = (datetime.datetime(int(a_year), int(a_month), int(a_day), int(a_hours), int(a_minutes), int(a_seconds)) -
           datetime.datetime(int(u_year), int(u_month), int(u_day), int(u_hours), int(u_minutes), int(u_seconds))).total_seconds()
-    if int(ts) < 1209600:
-        raise PeriodNotCompleted("A Period of 14 is not completed")
+    if int(ts) < 604800:
+        remaining_hrs = 24 - (int((int(ts) % 86400)/3600))
+        remaining_days = 7 - int(ts/86400)
+        remaining_mins = 60 - (int((int(ts) % 86400)/60)) % 60
+        #print(f"Account created: {u_day}-{u_month}-{u_year}")
+        print(
+            f"\033[0;31mYour GitHub account is too new to run this script, Please come back after {remaining_days} days, {remaining_hrs} hrs and {remaining_mins} minutes.\033[0m")
+        raise PeriodNotCompleted
 else:
     print("No arguments passed")
 
