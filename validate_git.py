38577be5c49711ccf9d5618557121cc25a9d26ca
Shra1V32 <namanageshwar@outlook.com>
2022-09-29 14:17:31 +0530

    validate_git: Fix issues with `RemoteDisconnected` * There seems to an issue with worldtimeapi.org, So we shift to other API.

--- validate_git.py
+++ validate_git.py
@@ -14,15 +14,13 @@ if sys.argv[1] != '':
     git_id = sys.argv[1]
     data = requests.get(f"https://api.github.com/users/{git_id}")
     current_date = requests.get(
-        "http://worldtimeapi.org/api/timezone/Asia/Kolkata")
-    a_year, a_month, a_day = current_date.json()["datetime"][:10].split('-')
-    a_hours, a_minutes, a_seconds = current_date.json()[
-        "datetime"][11:19].split(':')
+        "https://www.timeanddate.com/scripts/ts.php")
+    current_date = current_date.content[:10]
     api_data = data.json()
     creation_date = api_data["created_at"]
     u_year, u_month, u_day = creation_date[:10].split('-')
     u_hours, u_minutes, u_seconds = creation_date[11:19].split(':')
-    ts = (datetime.datetime(int(a_year), int(a_month), int(a_day), int(a_hours), int(a_minutes), int(a_seconds)) -
+    ts = (datetime.datetime.fromtimestamp(int(current_date)) -
           datetime.datetime(int(u_year), int(u_month), int(u_day), int(u_hours), int(u_minutes), int(u_seconds))).total_seconds()
     if int(ts) < 604800:
         remaining_hrs = 24 - (int((int(ts) % 86400)/3600))
